368382eeb9db62eb0cf37a3f40d129466d443723
hector-crean <hectorcrean@gmail.com>
2023-04-17 03:24:28 +0100

    Basic header nav + dropdown with nested routes

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,11 +1,68 @@
+import {
+    Stack,
+} from "@/components/Stack";
+import { Tree } from "@/models/tree";
+import {
+    motion,
+    useScroll,
+    useSpring
+} from "framer-motion";
+import styles from './header.module.scss';
+
+import { NavigationMenuImpl } from "@/components/navigation-menu";
+import PfizerIcon from '@/svg/pfizer';
 import Link from "next/link";
 
-import styles from "./Header.module.scss";
 
-export const Header = () => {
-	return (
-		<div className={styles.header}>
-			<Link href="/">Home</Link>
-		</div>
-	);
-};
+
+interface HeaderProps {
+    nodes: Tree;
+    activePathname: string;
+
+}
+const Header = ({ nodes, activePathname }: HeaderProps) => {
+
+    // const containerRef = useRef<HTMLElement>(null)
+    const { scrollYProgress } = useScroll({
+    });
+
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+    });
+
+
+
+
+
+
+    const MotionStack = motion(Stack);
+
+
+
+    return (
+        <motion.header
+            className={styles.header}
+            variants={{
+                visible: { top: "0px" },
+                hidden: { top: "-100px" }
+            }}
+
+        >
+            <div className={styles.header_nav}>
+                <div className={styles.home}>
+                    <Link className={styles.home_link} href={'/sections'} passHref><PfizerIcon /></Link>
+                </div>
+
+                <nav className={styles.site_navigation}>
+                    <NavigationMenuImpl nodes={nodes} activePathname={activePathname} />
+                </nav>
+                <div className={styles.search}></div>
+            </div>
+            <motion.hr className={styles.progress_bar} style={{ scaleX }} />
+        </motion.header>
+    );
+}
+
+export { Header };
